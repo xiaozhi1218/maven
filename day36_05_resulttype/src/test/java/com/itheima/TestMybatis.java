2c4a9d10275f98d852e95d001570dc73cb205d40
chenyongzhi <chenyongzhijjk@163.com>
2021-08-14 19:10:57 +0800

    mybatis

--- day36_05_resulttype/src/test/java/com/itheima/TestMybatis.java
+++ day36_05_resulttype/src/test/java/com/itheima/TestMybatis.java
@@ -1,12 +1,14 @@
 package com.itheima;
 
 import com.itheima.dao.UserDao;
+import com.itheima.pojo.User;
 import com.itheima.pojo.UserInfo;
 import com.itheima.utils.SqlSessionFactoryUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 包名:com.itheima
@@ -19,6 +21,15 @@ public class TestMybatis {
     public void test01(){
         SqlSession sqlSession = SqlSessionFactoryUtil.openSqlSession();
         UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+        Long total = userDao.findTotal();
+        System.out.println(total);
+
+        User user = userDao.findById(9);
+        System.out.println(user);
+
+        Map userMap = userDao.findByUsername("jax");
+        System.out.println(userMap);
 
         List<UserInfo> userInfoList = userDao.findAllUserInfo();
 
